e2bf0c7ac2cbdacc81798c0aa97ece7d0099c9af
Petros Chatzitoulousis <petroshatt@gmail.com>
2024-05-02 13:06:07 +0200

    First commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from preprocessing import *
 
 
 if __name__ == '__main__':
-    x = open_image('../../RU/data/quant_good_2024-04-10_11-43-07/capture/quant_good_2024-04-10_11-43-07.hdr')
+    x = open_image('../../../RU/data/quant_good_2024-04-10_11-43-07/capture/quant_good_2024-04-10_11-43-07.hdr')
     x = x[:, :, :]
 
     x2 = neighbouring_summation(x)
